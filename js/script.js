81b093c4246d339c9aaa9b61809636e111215dd4
Jishnu Pradeep <jishnupradeep@gmail.com>
2018-05-19 15:16:00 -0500

    ver2.0

--- js/script.js
+++ js/script.js
@@ -9,25 +9,40 @@ function countdown(){
 
         // Get todays date and time
         var now = new Date().getTime();
-        
+
         // Find the distance between now an the count down date
         //var distance = countDownDate - now;
-        
+
         // Time calculations for hours, minutes and seconds
         //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
+
         // Output the result in an element with id="demo"
        // document.getElementById("demo").innerHTML = hours + "h : " + minutes + "m : " + seconds + "s ";
         document.getElementById("demo").innerHTML = minutes + "m : " + seconds + "s ";
 
         distance = distance - 1000;
-        
-        // If the count down is over, write some text 
-        //if (distance < 0) {
-        //    clearInterval(x);
-        //    document.getElementById("demo").innerHTML = "EXPIRED";
-        //}
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
     }, 1000);
 }
+
+
+function confirmation() {
+    var resID = Math.floor(Math.random() * (99999999-00000001) + 00000001);
+    document.getElementById("conf_code").innerHTML = resID;
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: resID,
+        width: 50,
+        height: 50,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+    })
+}
+
+
